4fd1018d073014875104d91b508c61135654b437
thanhoangz <xuanhoang.ks6@gmail.com>
2019-09-25 21:55:00 +0700

    25/9 cấu trúc tạm

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,14 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { CourseComponent } from './admin/content/category-management/courses/course/course.component';
 import { LoginComponent } from './login/login.component';
+import { AdminManagementComponent } from './admin/admin-management/admin-management.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     CourseComponent,
-    LoginComponent
+    LoginComponent,
+    AdminManagementComponent
   ],
   imports: [
     BrowserModule,
